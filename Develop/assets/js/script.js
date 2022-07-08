f3e5b360dcb6797f400202842d4298db4ce5b299
Cristobal Barberis <cristobal@barberis.com>
2022-07-08 14:01:04 -0400

    text area on click

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -14,13 +14,14 @@ var duration = moment.duration(endHour.diff(startHour));
 var hours = duration.asHours();
 
 for (var i = 0; i <= hours; i++) {
-  var row = $("<div>").addClass("row").attr("hour", startHour.format("hA")).attr("date", date);
+  var dateTime = date + " " + startHour.format("LT")
+  var row = $("<div>").addClass("row").attr("data-datetime", dateTime);
   
   var timeCol = $("<div>").addClass("col-1 time-block hour");
   var timeSpan = $("<span>" + startHour.format("hA") + "</span>");
   
-  var eventCol = $("<div>").addClass("col-10 textarea future");
-  var eventSpan = $("<span>"); 
+  var eventCol = $("<div>").addClass("col-10 future");
+  // var eventSpan = $("<span>"); 
 
   var saveCol = $("<div>").addClass("col-1 saveBtn");
   var saveSpan = $("<span><i class='fa-solid fa-floppy-disk fa-lg'></i></span>");
@@ -28,17 +29,27 @@ for (var i = 0; i <= hours; i++) {
   timeCol.append(timeSpan);
   row.append(timeCol);
 
-  eventCol.append(eventSpan);
+  // eventCol.append(eventSpan);
   row.append(eventCol);
-
 
   saveCol.append(saveSpan);
   row.append(saveCol);
   $("#calendar").append(row);
 
-
   startHour.add(1, 'hours')
-
-  
 }
 
+// event text was clicked
+$("#calendar").on("click", ".col-10", function() {
+  var text = $(this)
+    .text()
+    .trim();
+
+  // replace  element with a new textarea
+  var textInput = $("<textarea>")
+    .val(text);
+  $(this).append(textInput);
+
+  // auto focus new element
+  textInput.trigger("focus");
+});
