6eafc9a99d1f589ee4af97b712f3d932ef3e1f0d
Cristobal Barberis <cristobal@barberis.com>
2022-07-07 22:49:53 -0400

    Initial dates in JS

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,5 +1,14 @@
-// day = 
+var date = moment().format("dddd, MMMM Do");
+$("#currentDay").text(date);
+var date = new Date();
+var start = "9:00";
+var end = "17:00";
 
+var startHour = moment(date + ' ' + start);
+var endHour = moment(date + ' ' + end);
+var duration = moment.duration(endHour.diff(startHour));
+var hours = duration.asHours();
+console.log(hours);
 // for (var i = 0; i < array.length; i++) {
 //   const element = array[i];
   
